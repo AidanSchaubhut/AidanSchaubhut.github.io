e40e8b473f89b52ee63c0b9570317bff0e320af4
AidanSchaubhut <aidanjs1208@gmail.com>
2021-02-25 17:40:16 -0600

    finsihed todo 3

--- test-server.js
+++ test-server.js
@@ -1,9 +1,11 @@
-var http = require("http");
-var port = 8686;
+const http = require("http");
+let port = process.argv[2];
 
 http.createServer(function(req,res){
   // handle response
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello ');
-  res.end('World!');
+  res.write('It is way ');
+  res.end('too cold right now');
 }).listen(port);
+
+console.log(process.argv)
