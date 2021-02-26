6967faf7f0306bf1b774e49af7cb04c1d6fd2724
AidanSchaubhut <aidanjs1208@gmail.com>
2021-02-25 18:33:53 -0600

    finished todo 4

--- projects/are-you-being-served/requestServer.js
+++ projects/are-you-being-served/requestServer.js
@@ -1,19 +1,23 @@
 // requestServer.js file
 const http = require("http");
 const request = require("request");
+var args = process.argv.slice(2);
 var port = 8686;
 
 var server = http.createServer(function(req, res){
-    request("http://youtube.com", function(error, response, body){
+    var url = args[0] ? args[0] : "<a default url>";
+    request(url, function(error, response, body){
         if(!error && response.statusCode === 200){
             res.writeHead(200, {"Content-Type": 'text/html'})
             res.write(body);
-            
+            console.log("running");
+            res.end();
         } else{
             res.writeHead(response.statusCode, {"Content-Type": "text/plain"})
             res.write(error.toString());
+            res.end();
         }
-        res.end();
+
     });
 });
 server.listen(port);
