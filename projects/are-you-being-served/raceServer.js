e676a7460c434c36868ccf4831d01d40c886ed09
AidanSchaubhut <aidanjs1208@gmail.com>
2021-03-16 18:18:21 -0500

    finished project

--- projects/are-you-being-served/raceServer.js
+++ projects/are-you-being-served/raceServer.js
@@ -9,25 +9,50 @@ http.createServer(function(req, res) {
     let racers = ["Green Ambler", "Catalack", "Steel Runner", "G.I. Jogger"];
 
     // TODO 6: Get the start time for the race
-    
+    let d = new Date();
+    let startTime = d.getTime();
 
     // TODO 11: Make the whole thing parallel
-    async.series( 
+    async.parallel( 
         // TODO 8: Supply an array of functions
         [
-            
+           function one(callback){
+               wrapper(callback);
+           }, 
+           function two(callback){
+                wrapper(callback);
+            }, 
+            function three(callback){
+                wrapper(callback);
+            }, 
+            function four(callback){
+                wrapper(callback);
+            }
         ],
         function (error, results) {
             // TODO 9: add a callback function to the end of the async call to tally the results 
-
+            res.write("Results:\n");
+            var victoryOrder = sortTogether(racers, results); 
+            for (var i = 0; i < victoryOrder.length; i++){
+                res.write(victoryOrder[i]);
+                res.write("\n");
+            }
+            let dt = new Date();
+            let endTime = dt.getTime();
+            let raceTime = endTime - startTime;
+            res.write("raceTime");
         }
     );
     
 }).listen(port);
-
+// 
 // TODO 7: create a common function to be called by all functions in the array passed to the async function
 function wrapper(callback){
-
+    setTimeout(function()
+    {
+        var d = new Date();
+        callback(null, d.getTime());
+    }, Math.random()*1000);
 }
 
 function sortTogether(names, times) {
