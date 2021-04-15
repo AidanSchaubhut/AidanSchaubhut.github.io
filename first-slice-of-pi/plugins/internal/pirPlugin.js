543bdaffb8eb51a5280ba531bc1fb8dec3a5f18e
AidanSchaubhut <aidanjs1208@gmail.com>
2021-04-15 17:32:52 -0500

    adding first slice of pi to repo

---
+++ first-slice-of-pi/plugins/internal/pirPlugin.js
@@ -0,0 +1,26 @@
+var resources = require('./../../resources/model');
+var Gpio = require('onoff').Gpio;
+
+var sensor;
+var device = resources.pi.sensors.pir;
+
+function connectHardware(){
+	sensor = new Gpio(device.gpio, 'in', 'both');
+
+	sensor.watch(function (err, value){
+	if (err){
+		print("There was a error")
+	}
+	else{
+		device.value = !!value;
+	}
+	});
+};
+
+exports.start = function (params){
+	connectHardware();
+};
+
+exports.stop = function(){
+sensor.unexport();
+};
---
+++ first-slice-of-pi/wot-server.js
@@ -0,0 +1,17 @@
+var httpServer = require('./servers/http'),
+	resources = require('./resources/model');
+
+var pirPlugin = require('./plugins/internal/pirPlugin');
+var dhtPlugin = require('./plugins/internal/dhtPlugin');
+var server = httpServer.listen(resources.pi.port, function () {
+	console.log("Running the Pi on port " + resources.pi.port);
+});
+
+pirPlugin.start({});
+dhtPlugin.start({'frequency': 2000});
+
+process.on('SIGINT', function() {
+	pirPlugin.stop();
+	dhtPlugin.stop();
+	process.exit();
+});
